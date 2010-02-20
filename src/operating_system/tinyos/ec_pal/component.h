fa409524bd7e0926736870b87f022781276555f3
Alexander Bernauer <alex@copton.net>
2010-02-20 11:01:53 +0000

    fixed typo

--- src/operating_system/tinyos/ec_pal/component.h
+++ src/operating_system/tinyos/ec_pal/component.h
@@ -4,7 +4,7 @@
 #include "infra/debug.h"
 #include <stdlib.h>
 
-static void* handles[TC_NUMBEROF_THREADS];
+static void* handles[EC_NUMBEROF_THREADS];
 
 static void setHandle(void* handle)
 {
@@ -15,7 +15,7 @@ static void setHandle(void* handle)
 static ec_tid_t findThread(void* handle)
 {
 	ec_tid_t i;
-    for (i=0; i<TC_NUMBEROF_THREADS; i++) {
+    for (i=0; i<EC_NUMBEROF_THREADS; i++) {
         if (handle == handles[i]) {
             handles[i] = NULL;
             return i;
