e72c6082f3663661095702729c189747d3cbbbfa
Alexander Bernauer <alex@copton.net>
2010-02-25 11:32:15 +0100

    tool for determing the text size per function

--- scripts/measure.sh
+++ scripts/measure.sh
@@ -1,32 +1,83 @@
 #!/bin/bash
 
-results=`tempfile --suffix .txt`
+detailed_results=`tempfile --suffix .cvs --prefix detailed`
+total_results=`tempfile --suffix .cvs --prefix total`
 
-count_cycles()
+lib_section_size()
 {
-	app=$1
-	${TOOLCHAIN}/${CC_PREFIX}objdump -d $app | $ROOT/scripts/count-cycles.py -
+	lib=$1
+    echo "# sections' size for $lib"
+	${TOOLCHAIN}/${CC_PREFIX}size -t $lib #| perl -ne '@parts = split; print "$parts[0], $parts[1], $parts[2]\n"'
+    [ $? -eq 0 ] || exit 1
+    echo
 }
 
-measure_size()
+app_section_size()
 {
 	app=$1
+    echo "# sections' size for $app"
 	${TOOLCHAIN}/${CC_PREFIX}size $app | perl -ne '@parts = split; print "$parts[0], $parts[1], $parts[2]\n"'
+    [ $? -eq 0 ] || exit 1
+    echo
+}
+
+total_text_size()
+{
+    objectfile=$1
+    echo "# total text size for $objectfile"
+	${TOOLCHAIN}/${CC_PREFIX}objdump -d $objectfile | $ROOT/scripts/sum-text-size.py - -t
+    [ $? -eq 0 ] || exit 1
+    echo
+}
+
+detailed_text_size()
+{
+    objectfile=$1
+    echo "# detailed text size for $objectfile"
+	${TOOLCHAIN}/${CC_PREFIX}objdump -d $objectfile | $ROOT/scripts/sum-text-size.py -
+    [ $? -eq 0 ] || exit 1
+    echo 
+}
+
+detailed_cycles()
+{
+	app=$1
+    echo "# number of cycles per function for $app"
+	${TOOLCHAIN}/${CC_PREFIX}objdump -d $app | $ROOT/scripts/count-cycles.py -
+    [ $? -eq 0 ] || exit 1
+    echo
 }
 
 measure_app()
 {
 	path=$1
-	app_name=`basename $path`
-	app=$path/$app_name
+	app=$path/`basename $path`
 
-	echo "# memory consumption for $app" >> $results
-	measure_size $app >> $results 2>&1
-	echo >> $results
+    (
+    app_section_size $app 
+    total_text_size $app
+    ) >> $total_results 2>&1
 
-	echo "# cpu cycles for $app" >> $results
-	count_cycles $app >> $results 2>&1
-	echo >> $results
+    (
+    detailed_text_size $app 
+    detailed_cycles $app
+    ) >> $detailed_results 2>&1
+}
+
+measure_lib()
+{
+	path=$1
+	lib=$path/lib`basename $path`.a
+
+    (
+    lib_section_size $lib 
+    total_text_size $lib
+    ) >> $total_results 2>&1
+
+    (
+    detailed_text_size $lib 
+    detailed_cycles $lib
+    ) >> $detailed_results 2>&1
 }
 
 cd $ROOT
@@ -38,4 +89,9 @@ make all MEASURE=true
 measure_app "src/application/collect_and_forward/event-based"
 measure_app "src/application/collect_and_forward/generated"
 
-editor $results
+measure_lib "src/application/collect_and_forward/event-based"
+measure_lib "src/application/collect_and_forward/generated"
+measure_lib "src/operating_system/tinyos/ec_pal"
+
+editor $total_results
+echo "results are in $total_results and in $detailed_results"
---
+++ scripts/sum-text-size.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import fileinput
+import re
+import sys
+
+class Process(object):
+    def __init__(self):
+        self.function_begin = re.compile(r"^[0-9a-f]+ <([^>]*)>:\n$")
+        self.command = re.compile(r"^\s*[0-9a-f]+:\s*(([0-9a-f]{2} )+)\s*([a-z]+).*\n$")
+        self.current = None
+        self.sums = { }
+        self.total = 0
+
+    def __call__(self, line):
+        mo = self.function_begin.match(line)
+        if mo:
+            function = mo.group(1)
+#            print "summing up for function", function
+            self.sums[function] = 0
+            self.current = function
+            return
+    
+        if line == "\n":
+            self.current = None
+            return
+    
+        if self.current != None:
+            mo = self.command.match(line)
+            size = len(mo.group(1))
+            assert size % 3 == 0, "'%s'" % mo.group(1)
+            numberofBytes = size / 3
+            self.sums[self.current] += numberofBytes
+            self.total += numberofBytes
+
+    def dump_stats(self):
+        out = []
+
+        for function, sum in self.sums.iteritems():
+            out.append((function, sum))
+
+        out.sort()
+
+        sys.stdout.write("function, size\n")
+        for pair in out:
+            sys.stdout.write("%s, %d\n" % pair)
+
+    def dump_total(self):
+        sys.stdout.write("total, " + str(self.total) + "\n")
+
+process = Process()
+for line in fileinput.input(sys.argv[1]):
+    process(line)
+
+if len(sys.argv) > 2 and sys.argv[2] == "-t":
+    process.dump_total()
+else:
+    process.dump_stats()
